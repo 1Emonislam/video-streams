cee2461e9fe2c51edd71f18cdf0ac1af64a802bc
1emonislam <emonislamweb@gmail.com>
2022-07-21 10:56:18 +0600

    video streams

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,40 +1,55 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
-import dotenv from 'dotenv';
-dotenv.config({ path: './.env' })
-import logger from './utils/logger';
-import { connectDatabase, disconnectFromDatabase } from './config/database';
-import { CORS_ORIGIN } from './utils/constants';
-import helmet from 'helmet';
-//all routes here
-import userRoutes from './modules/user/user.route'
-const PORT = process.env.PORT || 5000;
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectToDatabase, disconnectFromDatabase } from "./utils/database";
+import logger from "./utils/logger";
+import { CORS_ORIGIN } from "./constants";
+import helmet from "helmet";
+import userRoute from "./modules/user/user.route";
+import authRoute from "./modules/auth/auth.route";
+import videoRoute from "./modules/videos/video.route";
+import deserializeUser from "./middleware/deserializeUser";
+
+const PORT = process.env.PORT || 4000;
+
 const app = express();
-//middlewares
+
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
- origin: CORS_ORIGIN,
- credentials: true
-}))
+app.use(
+  cors({
+    origin: CORS_ORIGIN,
+    credentials: true,
+  })
+);
 app.use(helmet());
-app.use('/api/users', userRoutes)
+app.use(deserializeUser);
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/videos", videoRoute);
+
 const server = app.listen(PORT, async () => {
- await connectDatabase();
- logger.info(`Server Listening at http://localhost:${PORT}`)
-})
+  await connectToDatabase();
+  logger.info(`Server listening at htp://localhost:${PORT}`);
+});
+
 const signals = ["SIGTERM", "SIGINT"];
+
 function gracefulShutdown(signal: string) {
- process.on(signal, async () => {
-  logger.info("Goodbye, got signal", signal)
-  server.close()
-  //disconnect from the 
-  await disconnectFromDatabase();
-  logger.info("My work here is done")
-  process.exit(0);
- })
+  process.on(signal, async () => {
+    logger.info("Goodbye, got signal", signal);
+    server.close();
+
+    // disconnect from the db
+    await disconnectFromDatabase();
+
+    logger.info("My work here is done");
+
+    process.exit(0);
+  });
 }
+
 for (let i = 0; i < signals.length; i++) {
- gracefulShutdown(signals[i]);
+  gracefulShutdown(signals[i]);
 }
